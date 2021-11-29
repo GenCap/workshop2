26b27aa3f1d40b03a57efd156067a43d104b110a
Åsa Jonsson <asaj71@gmail.com>
2021-11-29 14:41:16 +0100

    all added

---
+++ src/comp/ClassComp.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class ClassComp extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    };
+
+    state = {};
+
+    render() {
+        return ( <
+            div >
+
+            <
+            /div>
+        );
+    }
+}
+
+export default ClassComp;
