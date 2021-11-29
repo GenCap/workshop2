a50954180d9b82c9fa13aa48396b1760667662de
Åsa Jonsson <asaj71@gmail.com>
2021-11-29 15:04:32 +0100

    add all

---
+++ src/comp/DataTable.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const DataTable = () => {
+
+  //sstate
+  // useState
+  // useEffect
+
+  // nested componets
+  const TableHeader=()=>{
+    return;
+  }
+  const TableRow=()=>{
+    return;// pass the student list och people list
+  }
+  const TableAction=()=>{
+    return;
+  }
+  // js function
+  return (
+    <div className="container">
+      <h1>render the nested</h1>
+      
+    </div>
+  );
+};
+
+export default DataTable;
--- src/components/DemoRouter.js
+++ src/components/DemoRouter.js
@@ -1,108 +1,69 @@
 import React, { Fragment } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link , Redirect} from "react-router-dom";
 import CrudDemo from './CrudDemo';
 import PersonDetails from './PersonDetails';
 import UpdatePerson from './UpdatePerson';
 
 const DemoRouter = () => {
 
-    return ( <
-        Fragment >
-        <
-        Router >
-        <
-        Header / >
-        <
-        div className = "container" >
-        <
-        Switch >
-        <
-        Route exact path = "/"
-        component = { Welcome }
-        /> <
-        Route path = "/home"
-        component = { Home }
-        /> <
-        Route path = "/person"
-        component = { Person }
-        /> <
-        Redirect from = "/contact"
-        to = "" / >
-        <
-        Route path = "/about"
-        component = { About }
-        /> <
-        Route path = "/crud"
-        component = { CrudDemo }
-        /> <
-        Route path = "/details/:id"
-        component = { PersonDetails }
-        /> <
-        Route path = "/updates/:id"
-        component = { UpdatePerson }
-        />
+    return (
+        <Fragment>
+            <Router>
+                <Header />
+                <div className="container">
+                    <Switch>
+                        <Route exact path="/" component={Welcome} />
+                        <Route path="/home" component={Home} />
+                        <Route path="/person" component={Person} />
+                        <Redirect from="/contact" to=""/>
+                        <Route path="/about" component={About} />
+                        <Route path="/crud" component={CrudDemo} />
+                        <Route path="/details/:id" component={PersonDetails} />
+                        <Route path="/updates/:id" component={UpdatePerson} />
 
-        <
-        Route component = { NotFound }
-        /> <
-        /Switch> <
-        /div> <
-        /Router> <
-        /Fragment>
+                        <Route component={NotFound} />
+                    </Switch>
+                </div>
+            </Router>
+        </Fragment>
     );
 
 };
 
-const Welcome = () => < b > Welcome Page < /b>;
-const Home = () => < b > Home Page < /b>;
-const About = () => < b > About Us Page < /b>;
-const Person = () => < b > Person Page < /b>;
-const NotFound = () => < b > Page Not Found < /b>;
+const Welcome = () => <b>Welcome Page</b>;
+const Home = () => <b>Home Page</b>;
+const About = () => <b>About Us Page</b>;
+const Person = () => <b>Person Page</b>;
+const NotFound = () => <b>Page Not Found</b>;
 
 const Header = () => {
-    return ( <
-        nav className = "navbar navbar-expand-sm navbar-dark bg-dark" >
-        <
-        div className = "container-fluid" >
-        <
-        ul className = "navbar-nav me-auto" >
-        <
-        li className = "nav-item" >
-        <
-        Link className = "navbar-brand"
-        to = "/home" > Home < /Link> <
-        /li> <
-        li className = "nav-item" >
-        <
-        Link className = "navbar-brand"
-        to = "/" > Welcome < /Link> <
-        /li> <
-        li className = "nav-item" >
-        <
-        Link className = "navbar-brand"
-        to = "/person" > Person < /Link> <
-        /li> <
-        li className = "nav-item" >
-        <
-        Link className = "navbar-brand"
-        to = "/about" > About Us < /Link> <
-        /li> <
-        li className = "nav-item" >
-        <
-        Link className = "navbar-brand"
-        to = "/crud" > CRUD < /Link> <
-        /li> <
-        /ul> <
-        form >
-        <
-        Link className = "btn btn-primary"
-        to = "/person" > Sign Up < /Link> <
-        /form> <
-        /div>
+return (
+    <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+        <div className="container-fluid">
+            <ul className="navbar-nav me-auto">
+                <li className="nav-item">
+                    <Link className="navbar-brand" to="/home">Home</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="navbar-brand" to="/">Welcome</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="navbar-brand" to="/person">Person</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="navbar-brand" to="/about">About Us</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="navbar-brand" to="/crud">CRUD</Link>
+                </li>
+            </ul>
+            <form>
+                <Link className="btn btn-primary" to="/person">Sign Up</Link>
+            </form>
+        </div>
 
-        <
-        /nav>
-    )
+    </nav>
+)
 };
 
 export default DemoRouter;
